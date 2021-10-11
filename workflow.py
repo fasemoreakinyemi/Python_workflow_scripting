6a6ccea8222bb870fb2d903f34f68386b506691d
Tillsa <till.sauerwein@web.de>
2021-10-11 10:24:01 +0200

    Basic argument input workflow

--- workflow.py
+++ workflow.py
@@ -1,1 +1,28 @@
-print("hellow world")
+import sys
+function_names = sys.argv
+
+
+
+def hello_world():
+	print("hello world")
+
+def good_night():
+	print("good night world")
+
+def good_morning():
+	print("good morning world")
+
+def main():
+	functions = {'hello_world': hello_world,
+				'good_night': good_night,
+				'good_morning': good_morning}
+	if "all" in function_names:
+		print("Executing entire workflow")
+		for function_name in functions:
+			functions[function_name]()
+	else:
+		print(f"Executing functions {', '.join(sys.argv[1:])}")
+		for function_name in sys.argv[1:]:
+			functions[function_name]()
+
+main()
