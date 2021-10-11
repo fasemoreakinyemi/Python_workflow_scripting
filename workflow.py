46e3e941723ad041c39eef64079f6ad53dfb45a2
Tillsa <till.sauerwein@web.de>
2021-10-11 11:43:24 +0200

    Updated worklow

--- workflow.py
+++ workflow.py
@@ -1,27 +1,28 @@
 import sys
-function_names = sys.argv
-
-
 
 def hello_world():
-	print("hello world")
-
-def good_night():
-	print("good night world")
+	print("Hello world!")
 
 def good_morning():
-	print("good morning world")
+	print("Good morning world!")
+
+def good_night():
+	print("Good night world!")
+
+def good_morning():
+	print("Good morning world!")
 
 def main():
+	function_names = sys.argv
 	functions = {'hello_world': hello_world,
-				'good_night': good_night,
-				'good_morning': good_morning}
+				'good_morning': good_morning,
+				'good_night': good_night}
 	if "all" in function_names:
-		print("Executing entire workflow")
+		print("Executing entire workflow:")
 		for function_name in functions:
 			functions[function_name]()
 	else:
-		print(f"Executing functions {', '.join(sys.argv[1:])}")
+		print(f"Executing functions {', '.join(sys.argv[1:])}:")
 		for function_name in sys.argv[1:]:
 			functions[function_name]()
 
